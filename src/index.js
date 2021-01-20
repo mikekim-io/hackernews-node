d51d18f7a9035cb3188fe29a397162f9557379ca
Mike Kim <mikekim.info@gmail.com>
2021-01-20 15:58:12 -0500

    simply test query

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 const { ApolloServer } = require('apollo-server');
-
+const fs = require('fs');
+const path = require('path');
 //defines GraphQL schema
 //defines simple query with one field called info
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
+
+const typeDefs = fs.readFileSync(
+  path.join(__dirname, 'schema.graphql'),
+  'utf8'
+);
+
+//dummy data for links
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL',
+  },
+];
+let idCount = links.length;
 
 //resolver is the actual implementation of the GraphQL schema
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  //create a new post that creates a new link
+  Mutation: {
+    //post a link
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  },
+
+  //create resolver for all fields in link
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
   },
 };
 
